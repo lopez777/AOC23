6e750d06aa036109bbdce4cbf338b69c1818f147
MARCOS <lmarcoslopezl@gmail.com>
2023-12-02 21:22:04 -0800

    rename 1-1 dir

--- 1/FirstLast.java
+++ 1/FirstLast.java
@@ -8,8 +8,26 @@ public class FirstLast {
     try {
       Scanner myReader = new Scanner(new File("input.txt"));
       int totalSum = 0;
+      Map<String, String> writtenNumbers = new HashMap<>();
+      writtenNumbers.put("one", "1");
+      writtenNumbers.put("two", "2");
+      writtenNumbers.put("three", "3");
+      writtenNumbers.put("four", "4");
+      writtenNumbers.put("five", "5");
+      writtenNumbers.put("six", "6");
+      writtenNumbers.put("seven", "7");
+      writtenNumbers.put("eight", "8");
+      writtenNumbers.put("nine", "9");
+
       while(myReader.hasNextLine()) {
         String s = myReader.nextLine();
+
+        // replace writtenNumbers with numeric string equivalent
+        System.out.println(s);
+        for(Map.Entry<String, String> entry : writtenNumbers.entrySet())
+          s = s.replace(entry.getKey(), entry.getValue());
+        System.out.println(s);
+
         boolean isFirst = true;
         char firstDigit = Character.MIN_VALUE;
         Stack<Character> digits = new Stack<>();
